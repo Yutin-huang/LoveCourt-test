a407538dd8bfffe5f8fc519502ca3006b84918bd
Yutin-huang <judy830203@gmail.com>
2025-07-31 13:06:36 +0000

    ✨ 新增後台戀愛判決紀錄頁面

--- models/Verdict.js
+++ models/Verdict.js
@@ -5,23 +5,6 @@ const verdictSchema = new mongoose.Schema({
   crime: String,
   suggestion: String,
   judgement: String,
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-const Verdict = mongoose.model("Verdict", verdictSchema);
-export default Verdict;
-
-//
-import mongoose from "mongoose";
-
-const verdictSchema = new mongoose.Schema({
-  complaint: String,
-  crime: String,
-  suggestion: String,
-  judgement: String,
 }, {
   timestamps: true  // ✅ 自動加 createdAt / updatedAt
 });
